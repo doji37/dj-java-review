dd5533bbb6ed90a2955d2ae66bb22cdc6e3db398
dj <do.ji37@outlook.com>
2024-03-07 20:38:24 -0500

    commit

--- collection-review/src/main/java/com/cydeo/MapReview.java
+++ collection-review/src/main/java/com/cydeo/MapReview.java
@@ -5,27 +5,45 @@ import java.util.Map;
 
 public class MapReview {
     public static void main(String[] args) {
-        Map<Integer,String> map=new HashMap<>();
-        map.put(1, "Sergiu");
+        // Create HashMap
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1, "Sergio");
         map.put(2, "Moses");
+        System.out.println(map);
         System.out.println(map.get(2));
-        System.out.println(findFirstNonRepeating("deo ava deveoe"));
+
+        // Find first non-repeating char in a String
+        // ex. "Java Developer", output should be "J"
+        // Steps: create HashMap, (ch, value count), iterate and count chars, revisit HashMap, and first first count = 1
+        System.out.println(findFirstNonRepeating("deo java developer"));
+
     }
+
     public static Character findFirstNonRepeating(String str){
-        Map<Character,Integer> map=new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
         // for counting the chars
         int count;
+
         // counting chars and putting values in the hash map
         for(Character ch:str.toCharArray()){
-            if(map.containsKey(ch)) {
-                count=map.get(ch);
+            if(map.containsKey(ch)){
+                count = map.get(ch);
                 map.put(ch, count+1);
-            } else map.put(ch, 1);
+            } else {
+                map.put(ch, 1);
+            }
         }
-        // starting from string, check if the char count equals 1
+
+        // starting from String, check if the char count equals 1
         for(Character ch:str.toCharArray()){
-            if(map.get(ch)==1) return ch;
+            if(map.get(ch) == 1){
+                return ch;
+            }
         }
-        return null;
+        return Character.MIN_VALUE;
     }
+
+
+
+
 }
--- collection-review/src/main/java/com/cydeo/SetReview.java
+++ collection-review/src/main/java/com/cydeo/SetReview.java
@@ -11,10 +11,39 @@ public class SetReview {
 
         // 2. Add element
         mySet.add(new Student(7, "Ibadet"));
-        mySet.add(new Student(7, "Ibadet"));
-        mySet.add(new Student(7, "Ibadet"));
+        mySet.add(new Student(8, "Ahmet"));
+        mySet.add(new Student(9, "Muhabbet"));
+
+        System.out.println(mySet);
+        // Will add duplicate to set if Override hashCode is not implemented
+        System.out.println(mySet.add(new Student(9, "Muhabbet")));
+
+
+        Set<Integer> numSet = new HashSet<>();
+        numSet.add(1);
+        numSet.add(2);
+        System.out.println(numSet);
+        // Set does not allow duplicate values
+        System.out.println(numSet.add(2));
+
+        // Find first repeating char in a String
+        // Steps: Create hashset, iterate, add chars to hashset, if false (duplicate val) return char
+        System.out.println("First repeating character is: "+ firstRepeatingCharacter("java developer"));
 
 
 
+    }
+
+    public static Character firstRepeatingCharacter(String str){
+        // Create a hashmap
+        Set<Character> chars = new HashSet<>();
+
+        // Iteration return ch if add returns false
+        for(Character ch:str.toCharArray()){
+            if(!chars.add(ch))
+                return ch;
+
+        }
+        return null;
     }
 }
